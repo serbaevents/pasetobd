bf1cd7fc44b37b57e374af3551bd6a5e0a67e2b0
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-10-28 11:09:59 +0800

    tambahinCRUD

--- tambahan/Update.go
+++ tambahan/Update.go
@@ -15,38 +15,6 @@ import (
 // 	fmt.Println(datagedung)
 // }
 
-func TestGeneratePasswordHash(t *testing.T) {
-	password := "begitulah"
-	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
-	fmt.Println("Password:", password)
-	fmt.Println("Hash:    ", hash)
-
-	match := CheckPasswordHash(password, hash)
-	fmt.Println("Match:   ", match)
-}
-func TestGeneratePrivateKeyPaseto(t *testing.T) {
-	privateKey, publicKey := watoken.GenerateKey()
-	fmt.Println(privateKey)
-	fmt.Println(publicKey)
-	hasil, err := watoken.Encode("serbaevent", privateKey)
-	fmt.Println(hasil, err)
-}
-
-func TestHashFunction(t *testing.T) {
-	mconn := SetConnection("MONGOSTRING", "PASETO1")
-	var userdata User
-	userdata.Username = "serbaevent"
-	userdata.Password = "begitulah"
-
-	filter := bson.M{"username": userdata.Username}
-	res := atdb.GetOneDoc[User](mconn, "user", filter)
-	fmt.Println("Mongo User Result: ", res)
-	hash, _ := HashPassword(userdata.Password)
-	fmt.Println("Hash Password : ", hash)
-	match := CheckPasswordHash(userdata.Password, res.Password)
-	fmt.Println("Match:   ", match)
-
-}
 
 func TestIsPasswordValid(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "PASETO1")
